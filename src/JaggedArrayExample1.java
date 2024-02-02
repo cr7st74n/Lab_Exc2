8b52d73073c68db9c30a374baa0b74e500df3356
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2024-02-02 13:56:46 -0500

    lab 2 in arrays

---
+++ src/JaggedArrayExample1.java
@@ -0,0 +1,30 @@
+/*********************************************************/
+// Fully documented (Purpose, Names, etc. (10 pts)
+// *********************
+// *********************
+/*********************************************************/
+public class JaggedArrayExample
+{
+    public static void main(String[] args)
+    {
+// create a jagged array with three rows
+        int[][] jaggedArray = {
+
+                ********, // Initialize the first row (3 pts)
+                ********, // Initialize the second row (3 pts)
+                ******** // Initialize the third row (3 pts)
+};
+// loop through each row of the jagged array
+        for (**************************) (5 pts)
+        {
+// loop through each column of the current row
+            for (************************) (5 pts)
+            {
+// print the value at the current position in the array
+                System.out.print(jaggedArray[i][j] + " ");
+            }
+// move to the next line for the next row
+            System.out.println();
+        }
+    }
+}
---
+++ src/Parameters.java
@@ -0,0 +1,50 @@
+//********************************************************
+// Parameters.java
+//
+// This program illustrates the concept of **********
+// **********************
+// *****
+//
+//********************************************************
+import java.util.*;
+public class Parameters
+{
+    //-----------------------------------------------
+    // Calls the average and minimum methods with
+    // different numbers of parameters.
+    public void  numAverage(int number){
+        number/args.lenght;
+        System.out.println(number);
+    }
+    public void NoParametersFunction(){
+    }
+    //System.out.println();
+}
+
+    //-----------------------------------------------
+    public static void main(String[] args)
+    {
+        double mean1, mean2;
+        mean1 = average(**************);
+        mean2 = average(***********************);
+        System.out.println ("mean1 = " + mean1);
+        System.out.println ("mean2 = " + mean2);
+    }
+
+//----------------------------------------------
+// Returns the average of its parameters.
+//----------------------------------------------
+/* Now specify the variable array 'list'
+public static double average ( ********) (4 pts)
+{
+double result = 0.0;
+if (list.length != 0)
+{
+int sum = 0;
+for (int ********) //Use for-each (3 pts)
+sum += num;
+result = *******************; (5 pts)
+}
+return result;
+}
+}
